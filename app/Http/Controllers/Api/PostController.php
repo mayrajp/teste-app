9c67814316f9849d11727ccae13a8a2ca9926cd5
mayrajp <may.musique@gmail.com>
2023-08-25 16:01:08 -0300

    add posts crud

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
 use App\Services\PostService;
+use Exception;
 
 class PostController extends Controller
 {
@@ -13,11 +15,64 @@ class PostController extends Controller
     {
         $this->postService = new PostService();
     }
-    
+
     public function index()
     {
-        $response = $this->postService->getAllPosts();
+        try {
 
-        return response($response->json(), $response->status()); 
+            $response = $this->postService->getAllPosts();
+
+            return response($response->json(), $response->status());
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    public function show(int $id)
+    {
+        try {
+
+            $response = $this->postService->showPost($id);
+
+            return response($response->json(), $response->status());
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    public function store(PostRequest $request)
+    {
+        try {
+
+            $response = $this->postService->createPost($request);
+
+            return response($response->json(), $response->status());
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    public function update(PostRequest $request, int $id)
+    {
+        try {
+
+            $response = $this->postService->updatePost($request, $id);
+
+            return response($response->json(), $response->status());
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    public function destroy(int $id)
+    {
+        try {
+
+            $response = $this->postService->destroyPost($id);
+
+            return response($response->json(), $response->status());
+        } catch (Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 500);
+        }
     }
 }
--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
@@ -11,7 +12,43 @@ class PostService
     {
         $response = Http::withHeaders([
             'Accept' => 'application/json',
-        ])->get('https://jsonplaceholder.typicode.com/posts');
+        ])->get(env('SITE_URL'). '/posts');
+
+        return $response; 
+    }
+
+    public function showPost(int $id) : Response
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->get(env('SITE_URL'). '/posts/' . $id);
+
+        return $response; 
+    }
+
+    public function createPost(PostRequest $request) : Response
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->post(env('SITE_URL'). '/posts', $request->all());
+
+        return $response; 
+    }
+
+    public function updatePost(PostRequest $request, int $id) : Response
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->put(env('SITE_URL'). '/posts/' . $id, $request->all());
+
+        return $response; 
+    }
+
+    public function destroyPost(int $id) : Response
+    {
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+        ])->delete(env('SITE_URL'). '/posts/' . $id);
 
         return $response; 
     }
